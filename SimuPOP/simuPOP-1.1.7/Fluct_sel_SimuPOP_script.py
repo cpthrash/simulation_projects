025a0735c580e063f66fb937a8fe92c4b643f0b5
Colin Pierce <pier0273@umn.edu>
2016-04-20 10:49:55 -0500

    Added while count loop that allows for repetitions of the simulation.  Default set to 10

--- SimuPOP/simuPOP-1.1.7/Fluct_sel_SimuPOP_script.py
+++ SimuPOP/simuPOP-1.1.7/Fluct_sel_SimuPOP_script.py
@@ -10,8 +10,7 @@ used to uncover allele frequencies using the Moran model of overlapping generati
 
 import os, sys, types, time 
 import simuOpt 
-import simuPOP as sim # May want to remove 'as sim', as this is just a shortcut so that simulation functions in 
-# the script only need to be defined as 'sim.' instead of 'simuPOP.'
+import simuPOP
 
 #Start SimuPOP program
 
@@ -34,6 +33,9 @@ Ploidy=1  #  Ploidy.  Haploid = 1
 Fitness1=[1.005, 1]  # Fitness values for each allele in environment 1
 
 Fitness2=[1, 1.005]  # Fitness values for each allele in environment 2
+
+Repetitions=10  # Number of repetitions of the simulation to run
+
 
 # Calculate # generations at which allele frequency will be calculated based on total number of generations
 
@@ -60,64 +62,73 @@ Gen9=(Generations/10)+Gen8
 Gen10=(Generations/10)+Gen9
 
 
- # initialize Population
- # set population size, loci, ploidy
-
-pop = sim.Population(size=PopSize, loci=NumLoci, ploidy=Ploidy, 
-
-    # create fields where allele frequency and fitness values can be stored
-    infoFields=['alleleFreq', 'fitness'])
+ 
+def simuFluctuatingSelectionWF():
     
-    # Evolve the population!
-pop.evolve(
+    Count = 1
 
-    # Initial Operators
-    initOps = [ 
+    # Number of repetitions of the simulation to run
+    Reps = Repetitions  
 
-        # Sets initial allele frequencies
-        sim.InitGenotype(freq=[0.5, 0.5]) 
-        ],
+    while Count <= Reps:
 
-    # Pre-mating operators
-    preOps = [
+        # initialize Population
+        # set population size, loci, ploidy
+        pop = simuPOP.Population(size=PopSize, loci=NumLoci, ploidy=Ploidy, 
 
-        # "u" specifies Allele 1->allele 2 mutation rate, "v" is opposite
-        sim.SNPMutator(u=Mutation, v=Mutation), 
+            # create fields where allele frequency and fitness values can be stored
+            infoFields=['alleleFreq', 'fitness'])
+            
+        # Evolve the population!
+        pop.evolve(
 
-        # Fitness effects (i.e. the environment) change every 500 generations
-        sim.MaSelector(loci=0, fitness=Fitness1, begin=Gen0, end=Gen1), 
-        sim.MaSelector(loci=0, fitness=Fitness2, begin=Gen1, end=Gen2),
-        sim.MaSelector(loci=0, fitness=Fitness1, begin=Gen2, end=Gen3),
-        sim.MaSelector(loci=0, fitness=Fitness2, begin=Gen3, end=Gen4),
-        sim.MaSelector(loci=0, fitness=Fitness1, begin=Gen4, end=Gen5),
-        sim.MaSelector(loci=0, fitness=Fitness2, begin=Gen5, end=Gen6),
-        sim.MaSelector(loci=0, fitness=Fitness1, begin=Gen6, end=Gen7),
-        sim.MaSelector(loci=0, fitness=Fitness2, begin=Gen7, end=Gen8),
-        sim.MaSelector(loci=0, fitness=Fitness1, begin=Gen8, end=Gen9),
-        sim.MaSelector(loci=0, fitness=Fitness2, begin=Gen9, end=Gen10)
+            # Initial Operators
+            initOps = [ 
 
-    ],
+                # Sets initial allele frequencies
+                simuPOP.InitGenotype(freq=[0.5, 0.5]) 
+                ],
 
-    # # Random selection mating scheme because using haploid population without sex.  Mating parents are randomly selection from the parent population
-    matingScheme = sim.RandomSelection(),
+            # Pre-mating operators
+            preOps = [
 
-    # Post mating operators
-    postOps = [
+                # "u" specifies Allele 1->allele 2 mutation rate, "v" is opposite
+                simuPOP.SNPMutator(u=Mutation, v=Mutation), 
 
-        # calculate allele frequencies beginning at the (Generations/10)th generation and every (Generations/10)th generations thereafter
-        sim.Stat(alleleFreq=0, begin=0, step=Step), 
-
-        # Print allele frequencies, ".3f" refers to floating decimal point with three places, "\n" moves to the next line
-        sim.PyEval(r"'Generation: %.3f\n' % (gen)", step=Step),
-        sim.PyEval(r"'f(A): %.3f\n' % (alleleFreq[0][0])", step=Step),
-        sim.PyEval(r"'f(a): %.3f\n' % (alleleFreq[0][1])", step=Step),
-
+                # Fitness effects (i.e. the environment) change every 500 generations
+                simuPOP.MaSelector(loci=0, fitness=Fitness1, begin=Gen0, end=Gen1), 
+                simuPOP.MaSelector(loci=0, fitness=Fitness2, begin=Gen1, end=Gen2),
+                simuPOP.MaSelector(loci=0, fitness=Fitness1, begin=Gen2, end=Gen3),
+                simuPOP.MaSelector(loci=0, fitness=Fitness2, begin=Gen3, end=Gen4),
+                simuPOP.MaSelector(loci=0, fitness=Fitness1, begin=Gen4, end=Gen5),
+                simuPOP.MaSelector(loci=0, fitness=Fitness2, begin=Gen5, end=Gen6),
+                simuPOP.MaSelector(loci=0, fitness=Fitness1, begin=Gen6, end=Gen7),
+                simuPOP.MaSelector(loci=0, fitness=Fitness2, begin=Gen7, end=Gen8),
+                simuPOP.MaSelector(loci=0, fitness=Fitness1, begin=Gen8, end=Gen9),
+                simuPOP.MaSelector(loci=0, fitness=Fitness2, begin=Gen9, end=Gen10)
 
             ],
-    
-    # program run over set amount of generations
-    gen = Generations 
-)
+
+            # # Random selection mating scheme because using haploid population without sex.  Mating parents are randomly selection from the parent population
+            matingScheme = simuPOP.RandomSelection(),
+
+            # Post mating operators
+            postOps = [
+
+                # calculate allele frequencies beginning at the (Generations/10)th generation and every (Generations/10)th generations thereafter
+                simuPOP.Stat(alleleFreq=0, begin=0, step=Step), 
+
+                # Print allele frequencies, ".3f" refers to floating decimal point with three places, "\n" moves to the next line
+                simuPOP.PyEval(r"'Generation: %.0f\n' % (gen)", step=Step),
+                simuPOP.PyEval(r"'f(A): %.3f\n' % (alleleFreq[0][0])", step=Step),
+                simuPOP.PyEval(r"'f(a): %.3f\n' % (alleleFreq[0][1])", step=Step),
 
 
+                    ],
+            
+            # program run over set amount of generations
+            gen = Generations 
+        )
+# Run the simulation function!
+simuFluctuatingSelectionWF()
 
