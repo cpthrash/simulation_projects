6d11151d68561eb54047045d542de75a687b38d2
Colin Pierce <pier0273@umn.edu>
2016-07-18 13:04:16 -0500

    Updating simuPOP and bash scripts

--- SimuPOP/simuPOP-1.1.7/working_scripts/run_simuPOP_R.sh
+++ SimuPOP/simuPOP-1.1.7/working_scripts/run_simuPOP_R.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-"""This shell script calls on a Python script that simulates fluctuating selection in a haploid population, and redirects the output to an Rscript for plotting and printing of a PDF file.  It also directs the printed pdf file to a specific directory"""
+#This shell script calls on a Python script that simulates fluctuating selection in a haploid population, and redirects the output to an Rscript for plotting and printing of a PDF file.  It also directs the printed pdf file to a specific directory
 
 module load molpopgen # load 'molpopgen' in order to use MSstats to produce summary statistics from simulation data outfile
+
+module load R # load R
 
 # Run the simulation
 python Fluct_sel_SimuPOP_script.py
@@ -11,31 +13,43 @@ python Fluct_sel_SimuPOP_script.py
 var=`python Fluct_sel_SimuPOP_script.py | tail -n 1`
 
 # Where is outfile from simulation? 
-'''Need to update'''
+# Need to update
 simOutfile=`python Fluct_sel_SimuPOP_script.py | tail -n 1`
 
+base=$(basename "${simOutfile}" .txt) # Strip the path information from the filename, printing only the file name
+
+popSize=$(echo "${base}" | cut -f 2 -d '-')
+
+generations=$(echo "${base}" | cut -f 3 -d '-')
+
+lowerSelValue=$(echo "${base}" | cut -f 4 -d '-')
+
+upperSelValue=$(echo "${base}" | cut -f 5 -d '-')
+
 # Take simOutfile and calculate summary statistics using msstats
-simOutfile | msstats >msstats_sel_${selection}_mut_time_${mut_time}_out.txt
+cat "${simOutfile}" | msstats > MSstats_popSize_${popSize}_generations_${generations}_lowerSelValue_${lowerSelValue}_upperSelValue__${upperSelValue}_out.txt
 
-# Where is plot_MSstats.R (file containing summary statistics using MSstats)?
-'''need to update'''
-plot_MSstats.R=/Users/ColinPierce/Desktop/simulation_projects/SimuPOP/simuPOP-1.1.7/working_scripts
+# Where is MSstats_outfile? (file containing summary statistics from MSstats)?
+MSstats_outfile=/home/morrellp/pier0273/simulation_projects_MSI/scripts/MSstats_popSize_${popSize}_generations_${generations}_lowerSelValue_${lowerSelValue}_upperSelValue__${upperSelValue}_out.txt
 
-# If no file exists at plot_MSstats.R, exit with error
-if [[ ! -f "${plot_MSstats.R}" ]]; then echo "Failed to find plot_MSstats.R" >&2; exit 1; fi
+
+# If no file exists at MSstats, exit with error
+if [[ ! -f "${MSstats_outfile}" ]]; then echo "Failed to find MSstats_outfile" >&2; exit 1; fi
 
 #	Where should we put the PDF files that are printed from the Rscript?
-PDF_OUTDIR='/Users/ColinPierce/Desktop/simulation_projects/PDFs'
+PDF_OUTDIR='/home/morrellp/pier0273/simulation_projects_MSI/PDFs/PDFs'
+
 
 #	Make the PDF output directory and any missing parent directories
-mkdir -p "${PDF_OUTDIR}"
+#mkdir -p "${PDF_OUTDIR}"
 
 
+plot_MSstats.R=/home/morrellp/pier0273/simulation_projects_MSI/scripts/  # Where is the R script that plots the summary statistics?
 
 #	Check to see if Rscript is installed. If not, exit with error
 if ! $(command -v Rscript > /dev/null 2> /dev/null); then echo "Please install R and place in your PATH" >&2; exit 1; fi
 
-# Call on the R script of filename ''
-# Pass argument 1 to the Rscript
+# Call on the R script of filename 'plot_MSstats.R'
+# Pass argument 1 to the Rscript, which is the location of the MSstats_outfile
 # Pass argument 2 to the Rscript
-Rscript "${plot_MSstats.R}" "${HOME}/SFSCode/msstats_reps_${reps}_out.txt" "${PDF_OUTDIR}/MS_${reps}"
+Rscript "${plot_MSstats.R}" "${MSstats_outfile}" "${PDF_OUTDIR}/PDF_${popSize_${popSize}_generations_${generations}_lowerSelValue_${lowerSelValue}_upperSelValue__${upperSelValue}}"
